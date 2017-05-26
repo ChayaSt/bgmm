84cedb15d159205c4c66d1c23c9730908d82c962
Chaya Stern <chaya.stern@choderalab.org>
2017-05-25 23:42:00 -0400

    fixed dimensions mismatch

--- bgmm/gmm.py
+++ bgmm/gmm.py
@@ -12,34 +12,36 @@ import numpy as np
 from bgmm.distributions import (Dirichlet, Multinomial, Gaussian, InverseGamma)
 import copy
 from scipy.misc import logsumexp
+from tqdm import tqdm
 
 
 class State:
     def __init__(self, z, mu, sigma_aq_mu, sigma_sq_n, pi):
         self.z = z                     # Assignments (represented as an array with K assignment given as integer) (N)
+        self.counts = np.zeros(pi.shape)               #
         self.pi = pi                   # Mixture probabilities (K)
         self.mu = mu                   # cluster means (Dim x K)
-        self.sigma_sq_mu = sigma_aq_mu # Between cluster variance
+        self.sigma_sq_mu = sigma_aq_mu # Between cluster variance (hyperparameter on Gaussian prior for mu)
         self.sigma_sq_n = sigma_sq_n   # Within cluster variance
 
 
 class Model:
     def __init__(self, alpha, K, sigma_sq_mu_prior, sigma_sq_n_prior, dim=1):
-        self.alpha = alpha            # Parameter for Dirichlet prior over mixture probabilities (Dim x K)
+        self.alpha = alpha            # Parameter for Dirichlet prior over mixture probabilities (K)
         self.K = K                    # Number of components
         self.sigma_sq_mu_prior = sigma_sq_mu_prior  # Inverse Gamma
         self.sigma_sq_n_prior = sigma_sq_n_prior    # Inverse Gamma
         self.dim = dim               # dimension (default is 1)
 
     def cond_pi(self, state):
-        counts = np.bincount(state.z)
-        return Dirichlet(self.alpha + counts)
+        state.counts = np.bincount(state.z)
+        return Dirichlet(self.alpha + state.counts)
 
     def cond_z(self, state, X):
         # ToDo make sure the evidence is for all dimensions (that it works for multidimensional Gaussian)
         nax = np.newaxis
         prior = np.log(state.pi)
-        evidence = Gaussian(state.mu, state.sigma_sq_n).log_p(X)
+        evidence = Gaussian(state.mu[nax, :, :], state.sigma_sq_n).log_p(X[:, nax, :]).sum(2)
         post = prior + evidence
 
         # normalize
@@ -53,27 +55,32 @@ class Model:
         for k in range(self.K):
             idx = np.where(state.z == k)[0]
             if idx.size > 0:
-                h[k, :] = X[idx, :].sum(0) / state.sigma_sq_n[k]
-                lam[k, :] = idx.size / state.sigma_sq_n[k] +1. / state.sigma_sq_mu[k]
+                h[k, :] = X[idx, :].sum(0) / state.sigma_sq_n[k] # hyperparemter U_0 is zero
+                lam[k, :] = idx.size / state.sigma_sq_n[[k]] + 1. / state.sigma_sq_mu
             else:
                 h[k, :] = 0
-                lam[k, :] = 1. / state.sigma_sq_mu[k]
+                lam[k, :] = 1. / state.sigma_sq_mu
         return Gaussian(h/lam, 1./lam)
 
     def cond_sigma_sq_mu(self, state):
         ndim = state.mu.shape[1]
-        a = self.sigma_sq_mu_prior.a + \
+        a = self.sigma_sq_mu_prior.a_mu + \
             0.5 * self.K * ndim
-        b = self.sigma_sq_mu_prior.b + \
+        b = self.sigma_sq_mu_prior.b_mu + \
             0.5 * np.sum(state.mu ** 2)
         return InverseGamma(a, b)
 
     def cond_sigma_sq_n(self, state, X):
-        ndata, ndim = X.shape
-        a = self.sigma_sq_n_prior.a + \
+        # ToDo something is not working here. Try a Jefferey's prior? Or maybe do mu and sigma together?
+        ndata = state.counts
+        ndim = self.dim
+        b = np.zeros(self.K)
+        a = self.sigma_sq_n_prior.a_mu + \
             0.5 * ndata * ndim
-        b = self.sigma_sq_n_prior.b + \
-            0.5 * np.sum((X - state.mu[state.z, :]) ** 2)
+        for k in range(self.K):
+            idx = np.where(state.z == k)[0]
+            b[k] = self.sigma_sq_n_prior.b_mu[k] + \
+                0.5 * np.sum((X[idx] - state.mu[k]) ** 2)
         return InverseGamma(a, b)
 
     def joint_log_p(self, state, X):
@@ -88,27 +95,27 @@ class Model:
 class Sampler(object):
 
     def __init__(self, niter, model, state, X):
-        self.logger = {'pi': np.zeros((niter, state.pi.shape)),
-                       'mean': np.zeros((niter, state.mu.shape)),
-                       'sigma_sq_mu': np.zeros((niter, state.sigma_sq_mu.shape)),
-                       'sigma_sq_n': np.zeros((niter, state.sigma_sq_n.shape))}
+        self.logger = {'pi': np.zeros((niter, model.K)),
+                       'mean': np.zeros((niter, model.K, model.dim)),
+                       'sigma_sq_mu': np.zeros((niter, 1)),
+                       'sigma_sq_n': np.zeros((niter, model.K))}
         self.niter = niter
         self.state = copy.deepcopy(state)
         self.model = model
         self.X = X
 
     def gibbs_step(self):
-        self.state.pi = self.cond_pi(self.state).sample()
-        self.state.z = self.cond_z(self.state, self.X).sample()
-        self.state.mu = self.cond_mu(self.state, self.X).sample()
-        self.state.sigma_sq_mu = self.cond_sigma_sq_mu(self.state).sample()
-        self.state.sigma_sq_n = self.cond_sigma_sq_n(self.state, self.X).sample()
+        self.state.pi = self.model.cond_pi(self.state).sample()
+        self.state.z = self.model.cond_z(self.state, self.X).sample()
+        self.state.mu = self.model.cond_mu(self.state, self.X).sample()
+        self.state.sigma_sq_mu = self.model.cond_sigma_sq_mu(self.state).sample()
+        self.state.sigma_sq_n = self.model.cond_sigma_sq_n(self.state, self.X).sample()
 
     def sample(self):
-        for i in range(self.niter):
-            self.model.gibbs_step(self.state, self.X)
+        for i in tqdm(range(self.niter)):
+            self.gibbs_step()
             # update logger
             self.logger['pi'][i] = self.state.pi
             self.logger['mean'][i] = self.state.mu
-            self.logger['sigma_sq_mu'] = self.state.sigma_sq_mu
-            self.logger['sigma_sq_n'] = self.state.sigma_sq_n
+            self.logger['sigma_sq_mu'][i] = self.state.sigma_sq_mu
+            self.logger['sigma_sq_n'][i] = self.state.sigma_sq_n
