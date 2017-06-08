a23f694696658139b6391e99e81c0b80945f59e2
Chaya Stern <chaya.stern@choderalab.org>
2017-06-07 23:33:48 -0400

    added tests to setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,4 @@ setup(name='bgmm',
       description='Gibbs sampler for Gaussian Mixture Model',
       url='https://github.com/ChayaSt/bgmm',
       author='Chaya D. Stern',
-      packages=['bgmm'])
+      packages=['bgmm', 'bgmm.tests'])
